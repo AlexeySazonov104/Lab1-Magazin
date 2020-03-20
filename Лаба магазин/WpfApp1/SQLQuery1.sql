9d08d8481633f05dda6ef3266b0507c774c55705
AlexeySazonov104 <61266295+AlexeySazonov104@users.noreply.github.com>
2020-03-20 23:19:56 +0800

    Add files via upload

---
+++ Лаба магазин/WpfApp1/SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿USE [803ca1_Sazonov_A]
+CREATE  TABLE  Users( 
+    [id]  INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    [login] VARCHAR(60),
+	[password] VARCHAR(60)
+)
---
+++ Лаба магазин/WpfApp1/WpfApp1/BestProduct.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для BestProduct.xaml
+    /// </summary>
+    public partial class BestProduct : Window
+    {
+        public BestProduct()
+        {
+            InitializeComponent();
+        }
+        private void Singin_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            SingIn w1 = new SingIn();
+            w1.Show();
+        }
+
+        private void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            MainWindow w1 = new MainWindow();
+            w1.Show();
+        }
+
+        private void TopButtton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            BestProduct w1 = new BestProduct();
+            w1.Show();
+        }
+
+        private void CatalogButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Catalog w1 = new Catalog();
+            w1.Show();
+        }
+
+        private void FavoriteButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Favorite w1 = new Favorite();
+            w1.Show();
+        }
+
+        private void BasketButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            ShoppingBasket w1 = new ShoppingBasket();
+            w1.Show();
+        }
+    }
+}
